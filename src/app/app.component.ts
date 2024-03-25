693c26d451d6720b60002cf1a4f257bdc8840e36
monika <msms.monika@gmail.com>
2024-03-26 01:58:28 +0530

    initail commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CommonModule } from '@angular/common';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'msmsmonika.github.io';
+  title = 'Monika Singh';
+  tabs=[];
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./home-page/home-page.component').then(mod => mod.HomePageComponent) }, 
+    { path: 'home', loadComponent: () => import('./home-page/home-page.component').then(mod => mod.HomePageComponent) }
+];
